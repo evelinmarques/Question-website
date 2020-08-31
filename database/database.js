27e6386517c77ffc656954477989e3e5d2625f62
Evelin Marques <56482367+evelinmarques@users.noreply.github.com>
2020-08-31 15:02:53 -0300

    database

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-// integração com o Banco de Dados. Para isso em const connection preencha com o seu usuário e senha.
+// integração com o Banco de Dados. Para isso em const connection preencha com o seu usuário em "root" e senha em "suasenhaaqui".
 const connection = new Sequelize('question', 'root', 'suasenhaaqui',{
 host: 'localhost', // complete com o nome do host, no meu caso é localhost
 dialect: 'mysql'
